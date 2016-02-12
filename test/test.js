1870f77f1d6d4b9a81f12693162af50e28ff113d
SuperPaintman <SuperPaintmanDeveloper@gmail.com>
2016-02-13 00:46:01 +0500

    Added tests for other methods

--- test/test.js
+++ test/test.js
@@ -9,7 +9,7 @@ const I18n      = require('../lib/i18n');
 
 describe("I18n lib", () => {
   /** Add */
-  describe("#add(filename|obj, lang?)", () => {
+  describe("#add(filename|obj|arr, lang?)", () => {
     /** Object */
     it("should add words from object", function () {
       const i18n = new I18n({
@@ -221,6 +221,161 @@ describe("I18n lib", () => {
       assert.equal(i18n.get("hello", 'ru'), "привет");
       assert.equal(i18n.get("bye", 'ru'), "пока");
       assert.equal(i18n.get("your name", 'ru'), "ваше имя");
+    });
+
+    it("should return valid translation with other default lang", function () {
+      const i18n = new I18n({
+        default: 'ru'
+      });
+
+      i18n
+        .add({
+          "hello": "привет",
+          "your name": "ваше имя"
+        }, 'ru')
+        .add({"bye": "пока"}, 'ru')
+        .add({"bye": "tschüss"}, 'de');
+
+      /* en */
+      assert.equal(i18n.get("hello", 'en'), "hello");
+      assert.equal(i18n.get("bye", 'en'), "bye");
+      assert.equal(i18n.get("your name", 'en'), "your name");
+
+      /* de */
+      assert.equal(i18n.get("bye", 'de'), "tschüss");
+      // such word is undefined
+      assert.equal(i18n.get("hello", 'de'), "hello");
+      assert.equal(i18n.get("your name", 'de'), "your name");
+
+      /* ru */
+      assert.equal(i18n.get("hello"), "привет");
+      assert.equal(i18n.get("bye"), "пока");
+      assert.equal(i18n.get("your name"), "ваше имя");
+
+      assert.equal(i18n.get("hello", 'ru'), "привет");
+      assert.equal(i18n.get("bye", 'ru'), "пока");
+      assert.equal(i18n.get("your name", 'ru'), "ваше имя");
+    });
+  });
+
+  /* setLocale */
+  describe("#setLocale(lang)", () => {
+    it("should return valid translation", function () {
+      const i18n = new I18n({
+        default: 'en'
+      });
+
+      i18n
+        .add({
+          "hello": "привет",
+          "your name": "ваше имя"
+        }, 'ru')
+        .add({"bye": "пока"}, 'ru')
+        .add({"bye": "tschüss"}, 'de');
+
+      /* en */
+      i18n.setLocale('en');
+      assert.equal(i18n.get("hello"), "hello");
+      assert.equal(i18n.get("bye"), "bye");
+      assert.equal(i18n.get("your name"), "your name");
+
+      /* de */
+      i18n.setLocale('de');
+      assert.equal(i18n.get("bye"), "tschüss");
+      // such word is undefined
+      assert.equal(i18n.get("hello"), "hello");
+      assert.equal(i18n.get("your name"), "your name");
+
+      /* fi */
+      i18n.setLocale('fi');
+      // such language is undefined
+      assert.equal(i18n.get("hello"), "hello");
+      assert.equal(i18n.get("bye"), "bye");
+      assert.equal(i18n.get("your name"), "your name");
+
+      /* ru */
+      i18n.setLocale('ru');
+      assert.equal(i18n.get("hello"), "привет");
+      assert.equal(i18n.get("bye"), "пока");
+      assert.equal(i18n.get("your name"), "ваше имя");
+    });
+  });
+
+  /* getLocale */
+  describe("#getLocale()", () => {
+    it("should return valid result", function () {
+      const i18n = new I18n({
+        default: 'en'
+      });
+
+      i18n
+        .add({
+          "hello": "привет",
+          "your name": "ваше имя"
+        }, 'ru')
+        .add({"bye": "пока"}, 'ru')
+        .add({"bye": "tschüss"}, 'de');
+
+      assert.equal(i18n.getLocale(), "en");
+
+      /* en */
+      i18n.setLocale('en');
+      assert.equal(i18n.getLocale(), "en");
+
+      /* de */
+      i18n.setLocale('de');
+      assert.equal(i18n.getLocale(), "de");
+
+      /* fi */
+      i18n.setLocale('fi');
+      assert.equal(i18n.getLocale(), "en");
+
+      /* ru */
+      i18n.setLocale('ru');
+      assert.equal(i18n.getLocale(), "ru");
+
+      /* reset */
+      i18n.setLocale('ru');
+      i18n.resetLocale();
+      assert.equal(i18n.getLocale(), "en");
+    });
+  });
+
+  /* resetLocale */
+  describe("#resetLocale()", () => {
+    it("should return valid translation", function () {
+      const i18n = new I18n({
+        default: 'en'
+      });
+
+      i18n
+        .add({
+          "hello": "привет",
+          "your name": "ваше имя"
+        }, 'ru')
+        .add({"bye": "пока"}, 'ru')
+        .add({"bye": "tschüss"}, 'de');
+
+      /* en */
+      i18n.setLocale('en');
+      i18n.resetLocale();
+      assert.equal(i18n.get("hello"), "hello");
+      assert.equal(i18n.get("bye"), "bye");
+      assert.equal(i18n.get("your name"), "your name");
+
+      /* de */
+      i18n.setLocale('de');
+      i18n.resetLocale();
+      assert.equal(i18n.get("hello"), "hello");
+      assert.equal(i18n.get("bye"), "bye");
+      assert.equal(i18n.get("your name"), "your name");
+
+      /* ru */
+      i18n.setLocale('ru');
+      i18n.resetLocale();
+      assert.equal(i18n.get("hello"), "hello");
+      assert.equal(i18n.get("bye"), "bye");
+      assert.equal(i18n.get("your name"), "your name");
     });
   });
 });
