5c77b24058a2cab8d599ba5680ce47470c1e936a
SuperPaintman <SuperPaintmanDeveloper@gmail.com>
2016-02-15 03:53:12 +0500

    Fixed gulpfile for node 0.12

---
+++ gulpfile.es6.js
@@ -0,0 +1,64 @@
+'use strict';
+/** Requires */
+const fs        = require('fs');
+const path      = require('path');
+
+const gulp      = require('gulp');
+const ts        = require('gulp-typescript');
+const eslint    = require('gulp-eslint');
+
+const gutil     = require('gulp-util');
+const plumber   = require('gulp-plumber');
+const yaml      = require('js-yaml');
+
+/** Constants */
+const tsPath = {
+  from: [
+    "./typings/**/*.ts",
+    "./development/**/*.ts",
+    "!./node_modules/**/*"
+  ],
+  to: "./bin/"
+};
+
+/** Helps */
+function onError(err) {
+  if (err.toString) {
+    gutil.log(gutil.colors.red("Error"), err.toString());
+  } else {
+    gutil.log(gutil.colors.red("Error"), err.message);
+  }
+
+  this.end();
+}
+
+/** Tasks */
+gulp.task("build", () => {
+  return gulp.src(tsPath.from)
+    /** @todo: Пламбер перезапускает TS */
+    // .pipe(plumber({
+    //   errorHandler: onError
+    // }))
+    .pipe(ts(ts.createProject('tsconfig.json')))
+    .pipe(gulp.dest(tsPath.to));
+});
+
+gulp.task("lint", () => {
+  return gulp.src(tsPath.from)
+    /** @todo: Пламбер перезапускает TS */
+    // .pipe(plumber({
+    //   errorHandler: onError
+    // }))
+    .pipe(ts(ts.createProject('tsconfig.json')))
+    .pipe(eslint(yaml.load(
+      fs.readFileSync(path.join(__dirname, "./.eslintrc.yml"))
+    )))
+    .pipe(eslint.format())
+    .pipe(eslint.failOnError());
+});
+
+gulp.task("watch", () => {
+  gulp.watch(tsPath.from, ["build"]);
+});
+
+gulp.task("default", ["build", "lint"]);
